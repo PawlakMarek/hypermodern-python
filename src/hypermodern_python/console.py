0d3804636e239f8f1d176dd2f8e393db4fce8428
PawlakMarek <26022173+PawlakMarek@users.noreply.github.com>
2020-06-13 14:18:56 +0200

    feat: guess system language; default to en

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,3 +1,5 @@
+import locale
+import re
 import textwrap
 
 import click
@@ -5,17 +7,22 @@ import requests
 
 from . import __version__
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+language_code = locale.getdefaultlocale()[0][0:2]
+if language_code == None:
+    language_code = 'en'
+API_URL = "https://{}.wikipedia.org/api/rest_v1/page/random/summary".format(
+    language_code)
 
 
 @click.command()
 @click.version_option(version=__version__)
 @click.option('--lang',
-              default='en',
+              default=language_code,
               help='Two-letter language code in which get the summary page')
 def main(lang):
     """The hypermodern Python project."""
-    with requests.get(API_URL.replace("/en.", "/" + lang + ".")) as response:
+    with requests.get(re.sub(r'/[a-z][a-z]\.', '/' + lang + '.',
+                             API_URL)) as response:
         try:
             response.raise_for_status()
         except requests.HTTPError:
