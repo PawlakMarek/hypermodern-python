7a84b54a058b838a8a0ad520b23a4f1226476d32
PawlakMarek <26022173+PawlakMarek@users.noreply.github.com>
2020-06-14 19:25:24 +0200

    refactor: move Wikipedia client to separate module and change excaption on request failure

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,15 +1,11 @@
 import locale
-import re
 import textwrap
 
 import click
-import requests
 
-from . import __version__
+from . import __version__, wikipedia
 
 language_code = locale.getdefaultlocale()[0][0:2]
-API_URL = "https://{}.wikipedia.org/api/rest_v1/page/random/summary".format(
-    language_code)
 
 
 @click.command()
@@ -19,15 +15,7 @@ API_URL = "https://{}.wikipedia.org/api/rest_v1/page/random/summary".format(
               help='Two-letter language code in which get the summary page')
 def main(lang):
     """The hypermodern Python project."""
-    with requests.get(re.sub(r'/[a-z][a-z]\.', '/' + lang + '.',
-                             API_URL)) as response:
-        try:
-            response.raise_for_status()
-        except requests.exceptions.RequestException:
-            click.echo("Wikipedia API is unavailable.\n"
-                       "Please check your connection and try again")
-            return 1
-        data = response.json()
+    data = wikipedia.random_page()
 
     title = data["title"]
     extract = data["extract"]
